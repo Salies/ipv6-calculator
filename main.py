3f48f45d49813f7a3c5fd23e757ebf9ae42f4ff0
Daniel Serezane <serezane@protonmail.com>
2022-10-09 16:51:06 -0300

    fim

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
-# 2001:0DB8:0000:0000:BEBA:0000:00C0:00CA
+# Redes de Computadores II - FCT-UNESP
+# Atividade 1
+# Daniel Henrique Serezane Pereira
 
 from re import compile
 from sys import exit
 from textwrap import wrap
 
+# Fomrata um número hexadecimal 128 bits em um ipv6
 def format_ipv6(ip):
     return ':'.join(wrap(ip, 4)).upper()
 
-r = compile("(?:^|(?<=\s))(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))(?=\s|$)")
-a = input()
+# Verifica se uma string representa um número
+def verifica_numero(string, err):
+    try:
+        n = int(string)
+        return n
+    except ValueError:
+        print(err)
+        exit(1)
 
+# Imprime as redes rightmost e leftmost
+def print_redes(listas, cidr):
+    print()
+    for lista in listas:
+        print(lista['nome'])
+        for rede in lista['redes']:
+            print(rede + '/' + str(cidr))
+        print()
+
+# Expressão regular para validar um endereço IPV6
+r = compile("(?:^|(?<=\s))(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))(?=\s|$)")
+a = input("Insira um endereço IPV6: ")
+
+# Validando a entrada
 if(not r.fullmatch(a)):
-    print("ipv6 inválido")
+    print("IPV6 inválido.")
     exit(1)
 
+# Tratando abreviações do ipv6
 s = a.split(':')
 
 if('' in s):
@@ -28,27 +52,40 @@ for i, grupo in enumerate(s):
     s[i] = grupo.zfill(4)
 
 # String de grupos do ipv6 para números hexadecimais
-#h = [hex(int(g, 16)) for g in s]
 s = "".join(s)
 # Descobrindo o grupo do meu barra
-t_mask = 16
-n_subnets = 10
+t_mask = input("Escolha uma máscara (0-128): /")
+t_mask = verifica_numero(t_mask, "Máscara inválida.")
+n_subnets = input("Alocar quantas redes? ")
+n_subnets = verifica_numero(n_subnets, "Número inválido.")
 bit_subnets = len(bin(n_subnets - 1)[2:])
 # n = tamanho da máscara de saída
 n = t_mask + bit_subnets
-# Transformado o hexadecimal em número
-s_no = int(s, 16)
+print("\nAlocando", 2 ** bit_subnets, "redes", ("/" + str(n) + "."))
+# Transformado o hexadecimal em número e aplicando o CIDR
+s_no = int(s, 16) & (0xffffffffffffffffffffffffffffffff << (128 - t_mask))
 # Fazendo o RightMost e o LeftMost
 # e armazenando as redes resultantes
 c = "0" * (128 - n)
 offset = lambda a : int(a + c, 2)
+to_ip = lambda b : format_ipv6(hex(s_no | offset(b))[2:])
 r_res = []
 l_res = []
-for i in range(n_subnets):
+for i in range(2 ** bit_subnets):
     r = format(i, f'0{bit_subnets}b')
-    l = r[::-1]
-    r_res.append(format_ipv6(hex(s_no | offset(r))[2:]))
-    l_res.append(format_ipv6(hex(s_no | offset(l))[2:]))
+    r_res.append(to_ip(r))
+    l_res.append(to_ip(r[::-1]))
 
-print(r_res)
-print(l_res)
+print_redes(
+[
+    {
+        'nome': "Rightmost",
+        'redes': r_res
+    },
+    {
+        'nome': "Leftmost",
+        'redes': l_res
+    }
+],
+n
+)
